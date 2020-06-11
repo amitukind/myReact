569e974072608060b1da4c4b0b7aeafa165cc28b
Amit Verma <amitverma.ukind@gmail.com>
2020-06-11 18:25:32 +0530

    Refs Demo Callback Refs - Method 2

--- hello-world/src/components/RefsDemo.js
+++ hello-world/src/components/RefsDemo.js
@@ -4,15 +4,24 @@ class RefsDemo extends Component {
   constructor(props) {
     super(props);
     this.inputRef = React.createRef();
+    // Method 2 Callback Refs
+    this.cbRef = null;
+    this.setCbRef = (element) => {
+      this.cbRef = element;
+    };
   }
   componentDidMount() {
-    this.inputRef.current.focus();
+    // this.inputRef.current.focus();
+    if (this.cbRef) {
+      this.cbRef.focus();
+    }
   }
 
   render() {
     return (
       <div>
         <input type="text" ref={this.inputRef}></input>
+        <input type="text" ref={this.setCbRef}></input>
       </div>
     );
   }
