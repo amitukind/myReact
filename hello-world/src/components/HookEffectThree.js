ccc14305f326440f9c69db88d7e0d9fabbd51ac3
Amit Verma <amitverma.ukind@gmail.com>
2020-06-13 17:09:28 +0530

    Hooks : useEffect() : Conditional Effect

--- hello-world/src/components/HookEffectThree.js
+++ hello-world/src/components/HookEffectThree.js
@@ -2,14 +2,23 @@ import React, { useState, useEffect } from "react";
 
 function HookEffectThree() {
   const [counter, setCounter] = useState(0);
+  const [name, setName] = useState("");
 
   useEffect(() => {
     document.title = "Counter " + counter;
-  });
+    console.log("Setting Counter");
+  }, [counter]); // Second parameter is an array of variables that needs to be checked for change before executing Effect Hook
 
   return (
     <div>
       <button onClick={() => setCounter(counter + 1)}>Counter {counter}</button>
+      Name : {name}
+      <input
+        type="text"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      ></input>
     </div>
   );
 }
